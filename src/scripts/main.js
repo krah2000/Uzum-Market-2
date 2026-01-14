72ec066a24fc1b483accdd0e6b55630c081e98b4
akramovalisher3010 <akramovalisher3010@gmail.com>
2026-01-14 23:29:47 +0500

    changed add all

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -39,10 +39,10 @@ getAllProducts()
       centeredSlides: true,
       loop: true,
 
-      // autoplay: {
-      //   delay: 2000,
-      //   disableOnInteraction: false,
-      // },
+      autoplay: {
+        delay: 2000,
+        disableOnInteraction: false,
+      },
 
       pagination: {
         el: ".swiper-pagination",
